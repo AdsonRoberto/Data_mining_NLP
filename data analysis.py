48ffc66b4920f1318ee87882147076274f11f9b8
AdsonRoberto <adson.uece@gmail.com>
2022-06-17 16:38:33 -0300

    preparando o ambiente no colab

---
+++ data analysis.py
@@ -0,0 +1,24 @@
+
+#Explorando os logs
+#Metricas
+ #Nº mensagens do STUART e alunos
+ #Distribuição de mensagens por alunos
+ #Distribuição de quantidade de mensagens
+ #Nº PCD
+ #Proporção STUART/ALUNOS
+ #Top mensagens do STUART
+ #Série temporal mensagens de alunos/STUART (linhas separadas)
+ #Análise linguística de mensagens de alunos
+ #Análise do diálogo
+ #Agradecimentos ao STUART
+ #Análise de sentimento
+
+from google.colab import drive
+drive.mount('/content/drive', force_remount=True)
+
+!pip install unidecode
+
+!python -m spacy download pt_core_news_sm
+from unidecode import unidecode 
+
+!python3 -m spacy download pt
